704249d125b014e0ecb46cd9d66a848a12a686ed
Flaera <74739658+Flaera@users.noreply.github.com>
2021-09-28 11:29:48 -0300

    Update board_specs.py
    Added logic comands and actions actuators.

--- scripts/car_specs/board_specs.py
+++ scripts/car_specs/board_specs.py
@@ -1,19 +1,38 @@
 from bge import logic
 from data_files.car_general_infos import cars
 
-
 def Start(cont):
-	pass
-
-
+    pass
+    
+    
 def Update(cont):
-	curr_car = open(logic.expandPath("//data_files/car_selected.txt"), "r")
-	car = curr_car.split('\n')[0]
-	
-	print(cars.keys)
+    own = cont.owner
+    curr_scene_objs = logic.getCurrentScene().objects
+    
+    #Actions:
+    act_top_speed = cont.actuators["act_top_speed"]
+    act_acceleration = cont.actuators["act_accel"]
+    act_turning = cont.actuators["act_turning"]
+    act_nitro_fully = cont.actuators["act_nitro_fully"]
+    
+    car = open(logic.expandPath("//data_files/car_selected.txt"), 'r')
+    car_sel = car.split('\n')[0]
+    for i in cars.keys():
+        if i[0] == car_sel:
+            #DEVO COLOCAR OS ACTIONS AQUI!!!!!
+            
+            #Change specifications.
+            curr_scene_objs["top_speed"]["top_speed"] = cars[2]
+            curr_scene_objs["accel"]["acceleration"] = cars[3]
+            curr_scene_objs["turning"]["turning"] = cars[4]
+            curr_scene_objs["nitro_fully"]["nitro_fully"] = cars[5]
+            #Activated the actions:
+            cont.activate(act_top_speed)
+            cont.activate(act_acceleration)
+            cont.activate(act_turning)
+            cont.activate(act_nitro_fully)
+            
 
-
-
-
-
-	curr_car.close()
+    car.close()
+    
+    
