72c7cc056022899e364a0208b4cc9fc439fe01d2
Rafael <rafael2mcont@gmail.com>
2021-12-20 13:09:50 -0300

    Music player button of exit implemented

--- scripts/music_player2.py
+++ scripts/music_player2.py
@@ -1,4 +1,4 @@
-from bge import logic
+from bge import logic, events
 from scripts.music_system import MusicSystem
 from data_files.music_list2 import musics2
 from data_files.info_musics import info_musics
@@ -19,5 +19,12 @@ def Update(cont):
     #print("music:", music)
     childrens["name_music.001"]["Text"] = music[0]
     childrens["names_artists.001"]["Text"] = music[1]
+
+    keyboard = logic.keyboard
+    tap = logic.KX_INPUT_JUST_ACTIVATED
+    if (keyboard.events[events.ONEKEY] == tap):
+        cont.activate(cont.actuators["add_menu_opt"])
+        cont.activate(cont.actuators["add_mp1"])
+        cont.activate(cont.actuators["re_mp2"])
     
 
