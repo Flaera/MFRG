1eb289f7de96fc3eca730d6cbe5923da9a1497dd
Rafael <rafael2mcont@gmail.com>
2021-12-20 20:17:28 -0300

    Confirmation of data disponibility implemented

--- data_files/warnings.py
+++ data_files/warnings.py
@@ -1,7 +1,12 @@
 warnings = {'0': "AÉ!? VOCÊ QUER MESMO SAIR DO JOGO? " \
-           "\n\nTenha certeza que salvou o jogo dentro do mapa." \
+           "\n\nIsso acontece toda vez que você acessa o mapa." \
            "\nDados recentes poderão ser perdidos se você não tiver salvo." \
            "\n\nSe liga!",
            '1': "Um novo jogo será iniciado e dados antigos serão apagados.\n\nContinuar?",
-           '2': "Este é seu único carro. Não pode ser vendido."
+           '2': "Este é seu único carro. Não pode ser vendido.",
+           '3': "Ao jogar este jogo você concorda em ceder dados da sua jogatina\n" \
+                "aos desenvolvedores.\n" \
+                "Seus dados serão mantidos em sigilo e serão usados apenas para fins\n" \
+                "de pesquisa acadêmica.\n" \
+                "Quer continuar jogando?"
            }
