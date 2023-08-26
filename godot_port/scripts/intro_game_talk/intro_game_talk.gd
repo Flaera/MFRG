742c88077a11d6748178f9b0c2e8b69030f8675f
flaera <rafael2mcont@gmail.com>
2023-08-26 12:11:36 -0300

    Dialog improvements and development dialog of event1 of character 1

--- godot_port/scripts/intro_game_talk/intro_game_talk.gd
+++ godot_port/scripts/intro_game_talk/intro_game_talk.gd
@@ -6,6 +6,9 @@ var index_dic: String
 var index: int
 var lenght: int
 var bg: Object
+var delta_time: float = 0.0
+var delta_inc: float = 0.0
+
 
 func _ready():
 	bg = preload("res://assets/blender2.79_old/assets/talk_scenes/backgrounds/bg_intro_scene.png") 
@@ -14,23 +17,36 @@ func _ready():
 	index = 0
 	lenght = len(talk[index_dic])
 
-	get_node("Sprite/TextureRectBG").set_texture(bg)
-	get_node("VBoxContainer/HBoxContainer/Button").align=true
-	get_node("VBoxContainer/HBoxContainer/Button").grab_focus()
+	get_node("CanvasLayer/Sprite/TextureRectBG").set_texture(bg)
+	get_node("CanvasLayer/ColorRect/VBoxContainer2/HBoxContainer/Button").align=true
+	get_node("CanvasLayer/ColorRect/VBoxContainer2/HBoxContainer/Button").grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	print("index=", index, "len=",lenght)
+func _process(_delta):
+	#print("index=", index, "len=",lenght)
 	if index==lenght:
 		index=-1
 		get_tree().change_scene("res://scenes/main_menu/main_menu.scn")
 	
-	get_node("VBoxContainer/HBoxContainer/LabelNameCharacter").text=talk[index_dic][index][0]
-	get_node("VBoxContainer/LabelText").text=talk[index_dic][index][1]
+	get_node("CanvasLayer/ColorRect/VBoxContainer2/HBoxContainer/LabelNameCharacter").text=talk[index_dic][index][0]
+	var text_char = get_node("CanvasLayer/ColorRect/VBoxContainer2/LabelText")
+	text_char.text=talk[index_dic][index][1]
 	
+	if (delta_time<0.5):
+		delta_time += _delta
+	elif (delta_time>=0.5 and delta_inc<1.0):
+		delta_inc += 0.01
+	else:
+		delta_time = float(0.0)
+	text_char.percent_visible = delta_inc
+	print("delta_inc=",delta_inc)
 
 
 func _on_Button_pressed():
-	if (index<lenght):
+	if (delta_inc<1.0):
+		delta_time = float(0.0)
+		delta_inc = float(1.0)
+	elif (delta_inc>=1.0 and index<lenght):
 		index+=1
+		delta_inc = float(0.0)
